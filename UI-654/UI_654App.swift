7ac0532c5568cbb5cd315c02624e4582fddce5a6
nyannya0328 <thecuba54@gmail.com>
2022-08-27 16:23:06 +0900

    Initial Commit

---
+++ UI-654/UI_654App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_654App.swift
+//  UI-654
+//
+//  Created by nyannyan0328 on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct UI_654App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
